5fa773103d5108423a7f6781efefa977aaa365fe
johnnylinares <johnnylinares26@gmail.com>
2025-07-14 13:49:38 -0400

    Reorganization

--- data/google_sheets.py
+++ data/google_sheets.py
@@ -1,12 +1,12 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from config.settings import SHEET_ID
+from config.settings import SHEET_ID, GOOGLE_CREDENTIAL
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets'
 ]
 
-creds = Credentials.from_service_account_file('data/alert-20-binance-telegram-bot.json', scopes=scopes)
+creds = Credentials.from_service_account_file(GOOGLE_CREDENTIAL, scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet = client.open_by_key(SHEET_ID)
