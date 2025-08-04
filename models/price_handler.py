1e49b95c9d3a881678158d637a7e76a1bd6a6592
johnnylinares <johnnylinares26@gmail.com>
2025-08-04 08:05:00 -0400

    Test threshold 0.1%

--- models/price_handler.py
+++ models/price_handler.py
@@ -8,7 +8,7 @@ from collections import defaultdict, deque
 from models.alert_handler import send_alert
 
 # ===== CONSTANTS =====
-THRESHOLD = 20  # % alert's change
+THRESHOLD = 0.1  # % alert's change
 TIME_WINDOW = 2 * 60 * 60  # 2 hours in seconds
 MAX_BATCH_SIZE = 200  # Maximum symbols per stream (Binance limit)
 
