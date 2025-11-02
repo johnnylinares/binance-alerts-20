d8de45211c92e46169fffa6413be42e0e4eb78f9
johnnylinares <johnnylinares26@gmail.com>
2025-11-02 15:34:42 -0400

    Yes

--- models/alert_handler.py
+++ models/alert_handler.py
@@ -10,6 +10,6 @@ bot = telegram.Bot(token=os.getenv("BOT_TOKEN"))
 async def alert_handler(symbol, percentage_change, price, emoji, volume):
     await bot.send_message(
         chat_id=os.getenv("CHANNEL_ID"),
-        text=f'{emoji[0]} #{symbol} {emoji[1]} {percentage_change:+.2f}%\n💵 ${price:.2f} 💰 ${volume}M'
+        text=f'{emoji[0]} #{symbol} {emoji[1]} {percentage_change:+.2f}%\n💵 ${price} 💰 ${volume}M'
     )
     print(f"{symbol} alert sended.")
